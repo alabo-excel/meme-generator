dcf1eb597d695652b143d004da74efa916acaf4c
Alabo Excel <iamalaboexcel@gmail.com>
2022-03-24 15:21:48 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App: React.FC = () => {
+  interface IData {
+    id: string;
+    name: string;
+    url: string;
+    width: number;
+    height: number;
+    box_count: number;
+  }
+
+  const [memes, setMemes] = useState<IData | any>([])
+  fetch('https://api.imgflip.com/get_memes')
+    .then(response => response.json())
+    .then(data => setMemes(data.data.memes))
+    .then(errors => console.log(errors))
+
+  // async function fetchMeme() {
+  //   const response = await fetch(`https://api.imgflip.com/get_memes`)
+  //   const json = await response.json();
+  //   // console.log(json)
+  //   return setMemes(json.data.memes);
+  // }
+  // fetchMeme()
+
+  const mapMeme = memes.map((meme: { name: string, url: string, id: number }) => {
+    return (
+      <div key={meme.id}>{meme.name}
+        <img width="200" src={meme.url} alt="" />
+      </div>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>MEME APP</h1>
+      </div>
+      {mapMeme}
     </div>
   );
 }
